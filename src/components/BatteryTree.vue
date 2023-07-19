0c58e4c1525e9720c3f8b68cf06a8af0c274a217
Michael Mulkern <merchantmariner04@yahoo.com>
2023-07-19 10:14:26 -0400

    Fixed logic in metal air

--- src/components/BatteryTree.vue
+++ src/components/BatteryTree.vue
@@ -960,11 +960,11 @@ export default {
                 this.showPackageWeight = false;
             } else if (this.howPacked == "loose" && this.transport == "Ground") {
                 this.openReport = true;
-            } else if (this.howPacked == "contained" && this.transport == "Air" && this.isMetal && ((this.battOrCell == "cell" && this.weightOfLi > 1) || (this.battOrCell == "battery" && this.weightOfLi > 2)) || this.packageWeight > 5) {
+            } else if (this.howPacked == "contained" && this.transport == "Air" && this.isMetal && (((this.battOrCell == "cell" && this.weightOfLi > 1) || (this.battOrCell == "battery" && this.weightOfLi > 2)) || this.packageWeight > 5)) {
                 this.gramsOption = this.battOrCell == "battery" ? this.gramsOption = "500 grams per battery" : this.gramsOption = "12 grams per cell";
                   this.showGramsQuestion = true;
                     this.showPackageWeight = false;     
-            } else if (this.howPacked == "contained" && this.transport == "Air" && this.isMetal && ((this.battOrCell == "cell" && this.weightOfLi <= 1) || (this.battOrCell == "battery" && this.weightOfLi <= 2)) && this.packageWeight <= 5) {
+            } else if (this.howPacked == "contained" && this.transport == "Air" && this.isMetal && (((this.battOrCell == "cell" && this.weightOfLi <= 1) || (this.battOrCell == "battery" && this.weightOfLi <= 2)) && this.packageWeight <= 5)) {
                 if(this.battOrCell == "cell"){
                     this.showFourBattAndButton = true;
                     this.showPackageWeight = false;
